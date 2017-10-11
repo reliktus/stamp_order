44f6c2427fc03adddc0402697a73f7b26cc4172c
relikt <reliktus@gmail.com>
2017-10-11 16:03:46 +0200

    View change, refactor, adding prodcuer signature

--- script/logotype.js
+++ script/logotype.js
@@ -3,7 +3,7 @@ function actionDisplayXeroModule() {
         '<h1 class=\"text-center text-info logo_font center\" >' +
         'Pięczątki On-Line - ' +
         '<a href=\"http://xerolublin.pl/\" target=\"_blank\">' +
-        '<img src=\"img/print.png\" title=\"Logo\">' +
+        '<img src=\"img/print.png\" title=\"Xero-Lublin Ewangelicka\">' +
         '</a>' +
         '</h1>';
         document.write(html);
