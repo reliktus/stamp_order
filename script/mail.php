6360ded3dcd8bfe9f6f5aba90b0b76274d093865
relikt <reliktus@gmail.com>
2017-10-12 15:31:43 +0200

    View change, refactor, adding prodcuer signature

--- script/mail.php
+++ script/mail.php
@@ -1,6 +1,6 @@
 <?php
 $to = "reliktus@gmail.com";
-$subject = "Zamowienie";
+$subject = "HTML email";
 
 $message = $_POST['mytext1'];
 
@@ -9,7 +9,7 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: multixero@gmail.com' . "\r\n";
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
 $headers .= 'Cc: myboss@example.com' . "\r\n";
 
 mail($to,$subject,$message,$headers);
